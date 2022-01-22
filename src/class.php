75a5f7f07081760ccff8b095c33724c6b56d88d3
Ali <97373457+DevNull-IR@users.noreply.github.com>
2022-01-22 15:16:57 +0330

    Create class.php

---
+++ src/class.php
@@ -0,0 +1,11 @@
+<?pho
+
+class ErrorLog {
+  public $FileNameErrorLog = "error.log";
+  public function __construct ($FileNameErrorLog){
+    $this->FileNameErrorLog = $FileNameErrorLog;
+  }
+  public function ErrorLog((string)$value){
+    file_put_contents($this->FileNameErrorLog,$value,FILE_APPEND);
+  }
+}
