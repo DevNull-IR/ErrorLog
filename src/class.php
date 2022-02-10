511adaec50b5ae6dd46bdc943aea45c1e0e310e1
Ali <97373457+DevNull-IR@users.noreply.github.com>
2022-02-10 14:45:31 +0330

    Update class.php

--- src/class.php
+++ src/class.php
@@ -1,14 +1,13 @@
 <?php
-namespace src\ErrorLog;
 class ErrorLog {
   public $FileNameErrorLog = "error.log";
-  public static function __construct ($FileNameErrorLog){
+  public function __construct ($FileNameErrorLog = "Error.log"){
     $this->FileNameErrorLog = $FileNameErrorLog;
   }
-  public static function __constructStatic ($FileNameErrorLog){
+  public function __constructStatic ($FileNameErrorLog){
     $this->FileNameErrorLog = $FileNameErrorLog;
   }
-  public static function ErrorLog((string)$value){
+  public function ErrorLog(string $value){
     file_put_contents($this->FileNameErrorLog,$value . PHP_EOL ,FILE_APPEND);
   }
 }
